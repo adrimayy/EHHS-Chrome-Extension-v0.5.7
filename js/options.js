e5582372bee3dc5dc92ac01147d7faf8278f7d8a
Adrimay <98491342+adrimayy@users.noreply.github.com>
2023-05-15 09:36:47 -0400

    Update options.js

--- js/options.js
+++ js/options.js
@@ -10,7 +10,6 @@
     ["Schedule", "/html/clockPage.html", "/images/clock.png","black"],
     ["SchoolTool", "https://easthamptonst.esboces.org/schooltoolweb/", "/images/schoolTool.png","color"],
     ["Naviance", "https://student.naviance.com/main", "/images/naviance.png","black"],    
-
     ["EHHS", "https://www.easthamptonschools.org/highschool", "/images/bonac.png","color"],
     ["Guidance","https://www.easthamptonschools.org/guidance", "/images/guidance.png","black"],
     ["Virtual Bulletin", "https://docs.google.com/presentation/d/1T-idGOxDysnA_qrA0ilTapPdZNXiYt-09Ew1kL1AUbk/preview?slide=id.gb047d9a6b6_0_28", "/images/announcement.png","black"],
@@ -45,9 +44,19 @@ let removedLinks = [
     ["Weather", "https://weather.com/en-BZ/weather/tenday/l/11937:4:US", "/images/weather.png","black"],
     
     // Science
+    ["Kami", "https://web.kamihq.com/web/viewer.html", "/images/kami.png","color"],
     ["EdPuzzle", "https://edpuzzle.com/", "/images/edpuzzle.png","color"],
     ["Castle Learning", "https://cl.castlelearning.com/Review/CLO/Account/LogOn", "/images/castleLearning.png","color"],
+    ["Khan Academy", "https://www.khanacademy.org/", "/images/khanAcademy.png","color"],
+    ["Duolingo", "https://www.duolingo.com/", "/images/duolingo.png","color"],
+    ["Rosetta Stone", "https://www.rosettastone.com/", "/images/rosettaStone.png","color"],
+    ["Oddessyware", "https://ehufsd.owschools.com/owsoo/login/auth", "/images/oddessyware.png","color"],
+    ["Word Reference", "https://www.wordreference.com/", "/images/wordReference.png","color"],
+    ["Verbix", "https://www.verbix.com/", "/images/verbix.png","color"],
+    ["Remind", "https://www.remind.com/", "/images/remind.png","color"],
+    ["AP Exam Dates","https://apcentral.collegeboard.org/exam-administration-ordering-scores/exam-dates", "/images/schedule.png","black"],
     ["AP Study Guides", "https://knowt.io/", "/images/knowt.png","color"],
+    ["AP Score Calculator", "https://www.albert.io/blog/ap-score-calculators/","/images/albert.png","color"],
 
     // Educational Games/Tools
     ["Common App", "https://apply.commonapp.org/dashboard", "/images/commonApp.png","color"],
@@ -57,7 +66,7 @@ let removedLinks = [
 
     // Music
     ["Sight Reading", "https://www.sightreadingfactory.com/sight-reading", "/images/sightReading.png","color"],
-    
+    ["Spotify", "https://open.spotify.com/", "/images/spotify.png","color"],
     //Phootgraphy and Art
     ["Pixlr", "https://pixlr.com/", "/images/pixlr.png","color"],
     
@@ -73,10 +82,8 @@ let removedLinks = [
     ["p5.js", "https://editor.p5js.org/", "/images/p5.png","color"],
     ["W3schools", "https://www.w3schools.com/", "/images/w3schools.png","color"],
     ["Tinkercad", "https://www.tinkercad.com/dashboard","/images/tinkercad.png","color"],
-    ["Khan Academy", "https://www.khanacademy.org/", "/images/khanAcademy.png","color"],
-
+    ["Astromech", "http://astromech.net/", "/images/astromech.png","color"],
     // Live Classes and educational games
-    ["Duolingo", "https://www.duolingo.com/", "/images/duolingo.png","color"],
     ["Quizlet", "https://quizlet.com/latest", "/images/quizlet.png","color"],
     ["Kahoot", "https://kahoot.it/", "/images/kahoot.png","color"],
     ["Blooket", "https://play.blooket.com/play", "/images/blooket.png","color"],
@@ -94,7 +101,7 @@ let removedLinks = [
 
     //YouTube Channels
     ["CrashCourse", "https://www.youtube.com/@crashcourse", "/images/crashCourse.png","color"],
-    ["Heilmer's History", "https://www.youtube.com/@heimlershistory", "/images/heilmer.png","color"],
+    ["Heimler's History", "https://www.youtube.com/@heimlershistory", "/images/heilmer.png","color"],
     ["Organic Chemistry Tutor", "https://www.youtube.com/@theorganicchemistrytutor", "/images/organicChemistryTutor.png","color"],
     ["Bozeman Science", "https://www.youtube.com/@Bozemanscience1", "/images/bozeman.png","color"],
     ["Flipping Physics", "https://www.youtube.com/@flippingphysics", "/images/flippingPhysics.png","black"],
@@ -103,17 +110,11 @@ let removedLinks = [
 
     ];
 
-// Get the checkbox element
 const checkbox = document.getElementById("addToTopCheckbox");
-
-// Get the current state of the checkbox and its checked attribute
 let isChecked = false; // set to unchecked by default
 let hasCheckedAttribute = checkbox.hasAttribute("checked");
-
-// Set the initial state of the checkbox
 checkbox.checked = isChecked;
 
-// Get the initial state of the checkbox from the storage API
 chrome.storage.sync.get("isChecked", ({ isChecked: storedIsChecked }) => {
   if (storedIsChecked !== undefined) {
     isChecked = storedIsChecked;
@@ -122,54 +123,39 @@ chrome.storage.sync.get("isChecked", ({ isChecked: storedIsChecked }) => {
   }
 });
 
-// Add an event listener to the checkbox
 checkbox.addEventListener("change", function() {
-  // Update the state of the checkbox
   isChecked = checkbox.checked;
   hasCheckedAttribute = checkbox.hasAttribute("checked");
-
-  // Save the state to the Chrome Extension Storage API
-  chrome.storage.sync.set({ isChecked: isChecked }, function() {
-    console.log("State saved");
-  });
-
-  // Update the checkbox's checked attribute
   if (isChecked) {
     checkbox.setAttribute("checked", "");
   } else {
     checkbox.removeAttribute("checked");
   }
+  updateDarkModeClass();
 });
 
-// Remove the checked attribute if the checkbox is unchecked
+
 if (!isChecked) {
   checkbox.removeAttribute("checked");
 }
-
-
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("sliderValue");
 output.innerHTML = slider.value;
 
-// Save the slider value to storage on every change
 slider.oninput = function() {
   output.innerHTML = this.value;
   chrome.storage.sync.set({ sliderValue: this.value });
+  updateDarkModeClass();
 }
 
-// Load the slider value from storage when the page is loaded
+
 chrome.storage.sync.get("sliderValue", function(data) {
   if (data.sliderValue) {
     slider.value = data.sliderValue;
     output.innerHTML = data.sliderValue;
   }
 });
-
-
-  const addedTableHeaders = ["Added Links"];
-  const removedTableHeaders = ["Removed Links"];
-
   const table = document.getElementById("addedLinksTable");
   const tbody = table.querySelector("tbody");
 
@@ -264,163 +250,221 @@ chrome.storage.sync.get("sliderValue", function(data) {
       addedLinks: JSON.stringify(addedLinks),
       removedLinks: JSON.stringify(removedLinks),
     });
-    console.log(addedLinks, removedLinks);
+    updateDarkModeClass(); 
+    loadTableData();
   }
-  function moveToRemovedWrapper() {
+  
+  function moveToRemovedWrapper(event) {
     moveToRemoved(event);
-    loadTableData();
+    updateDarkModeClass();
   }
   
-  function moveBackWrapper() {
+  function moveBackWrapper(event) {
     moveBack(event);
-    loadTableData();
+    updateDarkModeClass();
   }
   
+  function moveToRemoved(event) {
+    const target = event.target.tagName === 'BUTTON' ? event.target : event.target.parentElement;
+    const tr = target.parentElement;
+      
+    target.textContent = "+";
+    target.style.backgroundColor = "green";
+    target.removeEventListener("click", moveToRemovedWrapper);
+    target.addEventListener("click", moveBackWrapper);
+      
+    moveRow(tbody, removedLinksTbody, tr, addedLinks, removedLinks);
+  
+    updateDarkModeClass();
+  }
+  
+  
+  function moveBack(event) {
+    const target = event.target.tagName === 'BUTTON' ? event.target : event.target.parentElement;
+    const tr = target.parentElement;
+    
+    target.textContent = "−";
+    target.style.backgroundColor = "red";
+    target.removeEventListener("click", moveBackWrapper);
+    target.addEventListener("click", moveToRemovedWrapper);
+    
+    moveRow(removedLinksTbody, tbody, tr, removedLinks, addedLinks);
+    updateDarkModeClass();
 
-  function populateTable(tbody, links, addedTable) {
-    links.forEach(([linkName, url, image,lightDarkMode]) => {
-      const tr = document.createElement("tr");
-      const td = document.createElement("td");
-      const div = document.createElement("div");
-      const img = document.createElement("img");
-      const a = document.createElement("a"); // Create a new 'a' element
-      const button = document.createElement("button");
-      button.setAttribute("data-ignore", "true");
+  }
   
-      div.textContent = linkName;
-      img.src = image;
-      img.alt = lightDarkMode;
-      img.width = 50;
-      img.height = 50;
-      img.style.marginRight = "10px";
-  
-      img.setAttribute('data-url', url); // Add this line
-      a.href = url; // Set the 'href' attribute of the 'a' element to the URL
-      a.target = "_blank"; // Add this line to make the link open in a new tab
-      a.appendChild(img); // Append the 'img' element to the 'a' element
-        // Reset common CSS properties for the 'a' element
-      a.style.textDecoration = 'none';
-      a.style.color = 'inherit';
-      a.style.border = 'none';
-      a.style.padding = '0';
-      a.style.margin = '0';
-      a.style.display = 'inline-block';
-      a.style.background = 'none';
-      a.style.cursor = 'pointer';
-      a.style.textIndent = '0';
-      a.style.borderCollapse = 'unset';
-
-      button.style.minWidth = "50px";
-      button.style.height = "50px";
-      button.style.marginRight = "10px";
-      button.style.lineHeight = "43px";
-  
-      if (addedTable) {
-        button.textContent = "−";
-        button.style.backgroundColor = "red";
-        button.removeEventListener("click", moveBackWrapper);
-        button.addEventListener("click", moveToRemovedWrapper);
+  function updateDarkModeClass() {
+    const isDarkMode = document.documentElement.classList.contains("dark-mode");
+    const darkModeClass = 'dark-mode';
+    const images = document.querySelectorAll('img');
+    images.forEach(img => {
+      const lightDarkMode = img.getAttribute('alt');
+      if (isDarkMode && lightDarkMode === "black") {
+        img.classList.add(darkModeClass);
       } else {
-        button.textContent = "+";
-        button.style.backgroundColor = "green";
-        button.removeEventListener("click", moveToRemovedWrapper);
-        button.addEventListener("click", moveBackWrapper);
+        img.classList.remove(darkModeClass);
       }
-  
-      td.appendChild(a); // Append the 'a' element to the 'td' element instead of the 'img' element
-      td.appendChild(div);
-      tr.appendChild(td);
-      tr.appendChild(button);
-      tbody.appendChild(tr);
     });
   }
   
+function populateTable(tbody, links, addedTable, isDarkMode) {
+  links.forEach(([linkName, url, image, lightDarkMode]) => {
+    const tr = document.createElement("tr");
+    const td = document.createElement("td");
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    const a = document.createElement("a"); // Create a new 'a' element
+    const button = document.createElement("button");
+    button.setAttribute("data-ignore", "true");
 
-// Replace this block in your code with the following updated block
-//       img.setAttribute('data-url', url); // Add this line
-function moveToRemoved(event) {
-  const target = event.target.tagName === 'BUTTON' ? event.target : event.target.parentElement;
-  const tr = target.parentElement;
-  
-  target.textContent = "+";
-  target.style.backgroundColor = "green";
-  target.removeEventListener("click", moveToRemovedWrapper);
-  target.addEventListener("click", moveBackWrapper);
-  
-  
-  moveRow(tbody, removedLinksTbody, tr, addedLinks, removedLinks);
-}
-
-function moveBack(event) {
-  const target = event.target.tagName === 'BUTTON' ? event.target : event.target.parentElement;
-  const tr = target.parentElement;
-  
-  target.textContent = "−";
-  target.style.backgroundColor = "red";
-  target.removeEventListener("click", moveBackWrapper);
-  target.addEventListener("click", moveToRemovedWrapper);
-  
-  moveRow(removedLinksTbody, tbody, tr, removedLinks, addedLinks);
-}
+    div.textContent = linkName;
+    div.classList.add("linkName");
+    img.src = image;
+    img.alt = lightDarkMode;
+    img.width = 50;
+    img.height = 50;
+    img.style.marginRight = "10px";
+   
+    img.setAttribute('data-url', url); 
+    a.href = url; 
+    a.target = "_blank"; 
+    a.appendChild(img);
+    a.style.textIndent = '0';
+    a.style.width = '50px';
+    a.style.height = '50px';
 
 
-  function moveRow(fromTable, toTable, tr, fromLinks, toLinks) {
-    const linkName = tr.querySelector("div").textContent;
-    const imgElement = tr.querySelector("img");
-    const url = imgElement.getAttribute('data-url');
-    const image = imgElement.getAttribute('src');
-    const lightDarkMode = imgElement.getAttribute('alt');
+    button.style.minWidth = "50px";
+    button.style.height = "50px";
+    button.style.marginRight = "10px";
+    button.style.lineHeight = "43px";
 
-    const index = fromLinks.findIndex(([name]) => name === linkName);
-    if (index === -1) {
-      console.error(`Link data not found in the list`);
-      return;
-    }
-  
-    const movedLink = [linkName, url, image,lightDarkMode]; // Get the correct link data
-    fromLinks.splice(index, 1);
-  
-    const addToTopCheckbox = document.getElementById("addToTopCheckbox");
-    const addToTop = addToTopCheckbox.checked;
-  
-    if (addToTop) {
-      toLinks.splice(0, 0, movedLink); // Insert the moved link at the top of the destination list
-      toTable.insertBefore(tr, toTable.firstChild); // Move the row to the top of the destination table
+    if (addedTable) {
+      button.textContent = "−";
+      button.style.backgroundColor = "red";
+      button.removeEventListener("click", moveBackWrapper);
+      button.addEventListener("click", moveToRemovedWrapper);
     } else {
-      toLinks.push(movedLink); // Insert the moved link at the end of the destination list
-      toTable.appendChild(tr); // Move the row to the end of the destination table
+      button.textContent = "+";
+      button.style.backgroundColor = "green";
+      button.removeEventListener("click", moveToRemovedWrapper);
+      button.addEventListener("click", moveBackWrapper);
     }
-  
-    tr.parentElement.removeChild(tr); // Replace fromTable.removeChild(tr) with this line
-  
-    chrome.storage.local.set({
-      addedLinks: JSON.stringify(addedLinks),
-      removedLinks: JSON.stringify(removedLinks),
-    });
-    console.log(addedLinks, removedLinks);
-  }
-  
-  
 
+    button.onclick = function (event) {
+      event.stopPropagation();
+      if (addedTable) {
+        moveToRemovedWrapper(event);
+      } else {
+        moveBackWrapper(event);
+      }
+    };
+
+    td.appendChild(a); 
+    td.appendChild(div);
+    tr.appendChild(td);
+    tr.appendChild(button);
+    tbody.appendChild(tr);
+
+    const darkModeClass = 'dark-mode';
+    if (isDarkMode && lightDarkMode === "black") {
+      img.classList.add(darkModeClass);
+    } else {
+      img.classList.remove(darkModeClass);
+    }
+  });
+}
+checkbox.addEventListener("change", function() {
+  isChecked = checkbox.checked;
+  hasCheckedAttribute = checkbox.hasAttribute("checked");
+  if (isChecked) {
+    checkbox.setAttribute("checked", "");
+  } else {
+    checkbox.removeAttribute("checked");
+  }
+  updateDarkModeClass();
+});
+
+async function moveRow(fromTable, toTable, tr, fromLinks, toLinks) {
+  const linkName = tr.querySelector("div").textContent;
+  const imgElement = tr.querySelector("img");
+  const url = imgElement.getAttribute('data-url');
+  const image = imgElement.getAttribute('src');
+  const lightDarkMode = imgElement.getAttribute('alt');
+
+  const index = fromLinks.findIndex(([name]) => name === linkName);
+  if (index === -1) {
+    console.error(`Link data not found in the list`);
+    return;
+  }
+
+  const movedLink = [linkName, url, image, lightDarkMode]; // Get the correct link data
+  fromLinks.splice(index, 1);
+
+  const addToTopCheckbox = document.getElementById("addToTopCheckbox");
+  const addToTop = addToTopCheckbox.checked;
+
+  if (addToTop) {
+    toLinks.splice(0, 0, movedLink);
+    toTable.insertBefore(tr, toTable.firstChild);
+  } else {
+    toLinks.push(movedLink);
+    toTable.appendChild(tr);
+  }
+
+  const button = tr.querySelector("button");
+  button.onclick = null;
+
+  if (toTable === tbody) {
+    button.textContent = "−";
+    button.style.backgroundColor = "red";
+    button.addEventListener("click", moveToRemovedWrapper);
+  } else {
+    button.textContent = "+";
+    button.style.backgroundColor = "green";
+    button.addEventListener("click", moveBackWrapper);
+  }
+
+  chrome.storage.local.set({
+    addedLinks: JSON.stringify(addedLinks),
+    removedLinks: JSON.stringify(removedLinks),
+  });
+
+  const isDarkMode = document.documentElement.classList.contains("dark-mode");
+  const darkModeClass = 'dark-mode';
+
+  // Add or remove dark-mode class depending on the lightDarkMode value
+  if (isDarkMode && lightDarkMode === "black") {
+    tr.querySelector("img").classList.add(darkModeClass);
+  } else {
+    tr.querySelector("img").classList.remove(darkModeClass);
+  }
+
+  updateDarkModeClass();
+  loadTableData();
+}
+
+
+  
   function loadTableData() {
     tbody.innerHTML = "";
     removedLinksTbody.innerHTML = "";
-
-    populateTable(tbody, addedLinks, true);
-    populateTable(removedLinksTbody, removedLinks, false);
+  
+    const darkMode = document.documentElement.classList.contains("dark-mode");
+    populateTable(tbody, addedLinks, true, darkMode);
+    populateTable(removedLinksTbody, removedLinks, false, darkMode);
   }
+  
 
   await init();
 
   const resetButton = document.getElementById("resetButton");
   resetButton.addEventListener("click", function() {
-    // Reset checkbox
     checkbox.checked = false;
     isChecked = false;
     checkbox.removeAttribute("checked");
     chrome.storage.sync.set({ isChecked: false });
   
-    // Reset slider
     slider.value = 3;
     output.innerHTML = 3;
     chrome.storage.sync.set({ sliderValue: 3 });
@@ -429,5 +473,4 @@ function moveBack(event) {
     location.reload();
   });
   
-  console.log(addedLinks, removedLinks);
 })();
